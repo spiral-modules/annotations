8f4c19de864abb74bbb18794f0cdb82cd864a2dd
Wolfy-J <wolfy.jd@gmail.com>
2019-02-14 18:29:04 +0300

    init

---
+++ src/NodeInterface.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Annotations;
+
+class NodeInterface
+{
+    // todo: all nodes in a scalar form? keep scalar nodes separatelly?
+}
--- src/Parser.php
+++ src/Parser.php
@@ -9,7 +9,24 @@ declare(strict_types=1);
 
 namespace Spiral\Annotations;
 
+use Doctrine\Common\Lexer\AbstractLexer;
+
 class Parser
 {
+    // Embedded node types
+    public const STRING  = 1;
+    public const INTEGER = 2;
+    public const FLOAT   = 3;
+    public const BOOL    = 4;
+    public const ARRAY   = 5;
 
+    /** @var NodeInterface[] */
+    private $nodes = [];
+
+    private $lexer;
+
+    public function __construct(AbstractLexer $lexer)
+    {
+        $this->lexer = $lexer;
+    }
 }
